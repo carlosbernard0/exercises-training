87f009eb02889cb9372379ad8d3ec540cee4d7d9
carlos <carlosbernardo774@gmail.com>
2024-02-19 10:11:45 -0300

    53 solved

--- src/SumOfConsecutiveOddNumbersI.java
+++ src/SumOfConsecutiveOddNumbersI.java
@@ -1,30 +1,27 @@
 import java.util.Scanner;
 
-public class SumOfConsecutiveOddNumbersI {
+public class SumOfConsecutiveOddNumbersI{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         int x = input.nextInt();
         int y = input.nextInt();
-        int soma = 0;
 
-
+        int odd = 0;
 
         if (x>y){
             for (int i = y+1; i < x; i++) {
                 if (i % 2 != 0){
-                    soma += i;
+                    odd += i;
                 }
             }
         }else {
-            for (int i = x; i < y; i++) {
+            for (int i = x+1; i < y; i++) {
                 if (i % 2 != 0){
-                    soma += i;
+                    odd += i;
                 }
             }
         }
-        System.out.println(soma);
-        input.close();
-
+        System.out.println(odd);
     }
 }
